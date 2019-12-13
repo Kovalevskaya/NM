85e45c12e0bfb5a34dba524910b3f01aa22b7c3f
Kovalevskaya <58827476+Kovalevskaya@users.noreply.github.com>
2019-12-13 11:59:37 +0300

    lab

---
+++ Lab_1_method_LU.py
@@ -0,0 +1,27 @@
+﻿# Решение системы линейных уравнений с трехдиагональной матрицей
+import numpy as np
+
+A=np.array([[435,4,0,0],[8,564,7,0],[0,5,395,33],[0,0,6,345]])
+b=np.array([24,35,63,96])
+
+
+def sol(A,b):
+	d=np.array([A[i,i] for i in range(len(A))],float)
+	l=np.array([A[i,i+1] for i in range(len(A)-1)],float)
+	c=np.array([A[i+1,i] for i in range(len(A)-1)],float)
+	r1=np.zeros(len(A),float)
+	r1[1]=-l[0]/d[0]
+	for i in range(2,len(A)):
+		r1[i]=-l[i-1]/(d[i-1]+c[i-1]*r1[i-1])
+	r2=np.zeros(len(A),float)
+	r2[1]=b[0]/d[0]
+	for i in range(2,len(A)):
+		r2[i]=(-c[i-1]*r2[i-1]+b[i-1])/(d[i-1]+c[i-1]*r1[i-1])
+	x=np.zeros(len(A),float)
+	x[-1]=(-c[-1]*r2[-1]+b[-1])/(d[-1]+c[-1]*r1[-1])
+	for i in range(len(A)-2,-1,-1):
+		x[i]=r1[i+1]*x[i+1]+r2[i+1]
+	return x
+    
+x = sol(A,b)
+print(np.dot(A,x))
---
+++ Newton method.py.py
@@ -0,0 +1,43 @@
+﻿#Метод Ньютона
+import numpy as np
+
+def F(x):
+	y = np.zeros_like(x)
+	y[0] = (3 + 2*x[0])*x[0] - 2*x[1] - 3
+	y[1:-1] = (3 + 2*x[1:-1])*x[1:-1] - x[:-2] - 2*x[2:] -2
+	y[-1] = (3 + 2*x[-1])*x[-1] - x[-2] - 4
+	return y
+
+def J(x):
+	n = len(x)
+	b = np.zeros((n, n))
+	b[0, 0] = 3 + 4*x[0]
+	b[0, 1] = -2
+	for i in range(n-1):
+		b[i, i-1] = -1
+		b[i, i] = 3 + 4*x[i]
+		b[i, i+1] = -2
+	b[-1, -2] = -1
+	b[-1, -1] = 3 + 4*x[-1]
+	return b
+
+def Sol(F, J, guess):
+	N = len(guess)
+	d = np.ones(N)
+	x = np.array(guess, float)
+	acc = 0.001
+	k = 0
+	while max(abs(d)) > acc and k < 100:
+		d = np.linalg.solve(J(x), -F(x))
+		x = x + d
+		k += 1
+	return x, k
+
+n = 10
+guess = 3*np.ones(n)
+sol, its = Sol(F, J, guess)
+
+if its > 0:
+	print("x = {}".format(sol))
+else:
+	print("Решение не найдено!")
